ea2269e2ca4ea5dfdd19b02b591ad9d042d078f9
Robel <55069760+rbwondimu@users.noreply.github.com>
2019-12-12 20:38:29 -0500

    Add files via upload
    What an experience!

---
+++ ETL/queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE crime_2019(
+id INT PRIMARY KEY,
+report_date TEXT,
+shift TEXT,
+offense TEXT,
+block TEXT,
+ward TEXT
+);
+CREATE TABLE crime_2018 (
+id INT PRIMARY KEY,
+report_date TEXT,
+shift TEXT,
+offense TEXT,
+block TEXT,
+ward TEXT
+);
